8cf0eeaac833d27a8e457e87860a5d7245e1a8ba
harsh183 <hrshdeep51@gmail.com>
2020-08-08 12:45:32 -0500

    add two api endpoints for index and show for courses

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,20 @@
+class CoursesController < ApplicationController
+    before_action :set_course, only: [:show]
+
+    def index
+        courses = Course.all 
+        render json: courses
+    end
+
+    def show
+        render json: @course
+    end
+
+    # TODO: Remaining rest actions
+
+    private
+
+    def set_course
+        @course = Course.find(params[:id])
+    end
+end
