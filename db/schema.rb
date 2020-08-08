44f6403b415a4988a94085696ad4240dfa44847b
harsh183 <hrshdeep51@gmail.com>
2020-08-08 12:14:57 -0500

    create courses model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_08_165004) do
+ActiveRecord::Schema.define(version: 2020_08_08_171144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.string "professor"
+    t.text "notice_board"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "url"
