70b1c15c3e2a01cdae8080cc7357ceb8ef49b754
harsh183 <hrshdeep51@gmail.com>
2020-08-09 05:51:41 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,23 @@ first_course = Course.create(name: 'CS 125',
                              professor: 'Geoff Challen',
                              notice_board: 'Lectures every MWF 11am')
 
-first_course.rooms.create(url: 'https://example.com',
-                          capacity: 5,
-                          occupants: 3,
-                          purpose: 'Informal Group Study')
+10.times do |n|
+  first_course.rooms.create(url: 'https://example.com',
+                            capacity: 5,
+                            occupants: 3,
+                            purpose: "Informal Group Study #{n}")
+
+end
+
+15.times do |n|
+  course = Course.create(name: "MATH #{n}",
+                         professor: Faker::Name.name,
+                         notice_board: Faker::Lorem.paragraph)
+
+  5.times do
+    course.rooms.create(url: 'https://example.com',
+                        capacity: 50,
+                        occupants: rand(50),
+                        purpose: "Lecture #{n}")
+  end
+end
